f67c77be9d17378305fd74b53c97c88cf4d807bf
dianarussi <diana.russiposada@gmail.com>
2022-12-06 23:04:30 -0500

    Category model

---
+++ app/Http/Requests/Category/StoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryStoreRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' =>'required|string|max:50',
+            'description' =>'nullable|string|max:255',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required'=>'Este campo es requerido.',
+            'name.string'=>'El valor no es correcto.',
+            'name.max'=>'Solo se permiten 50 caracteres.',
+            'description.string'=>'El valor no es correcto.',
+            'description.max'=>'Solo se permiten 255 caracteres.',
+        ];
+    }
+}
